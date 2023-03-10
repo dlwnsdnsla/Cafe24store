a42def95c7406bf28c8ffc05345efea7a06dd511
dlwnsdnsla <122263039+dlwnsdnsla@users.noreply.github.com>
2023-03-10 16:41:30 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,49 @@
+
+$(".num1").on("click",function(){
+    $(".article1 .recommend1").css("display","block");
+    $(".article1 .recommend2").css("display","none");
+    $(".article1 .recommend3").css("display","none");
+    $(".article1 .recommend4").css("display","none");
+    $(".article1 .recommend5").css("display","none");
+    $(".article1 .recommend6").css("display","none");
+    });
+$(".num2").on("click",function(){
+    $(".article1 .recommend2").css("display","block");
+    $(".article1 .recommend1").css("display","none");
+    $(".article1 .recommend3").css("display","none");
+    $(".article1 .recommend4").css("display","none");
+    $(".article1 .recommend5").css("display","none");
+    $(".article1 .recommend6").css("display","none");
+    });
+$(".num3").on("click",function(){
+    $(".article1 .recommend3").css("display","block");
+    $(".article1 .recommend1").css("display","none");
+    $(".article1 .recommend2").css("display","none");
+    $(".article1 .recommend4").css("display","none");
+    $(".article1 .recommend5").css("display","none");
+    $(".article1 .recommend6").css("display","none");
+    });
+$(".num4").on("click",function(){
+    $(".article1 .recommend4").css("display","block");
+    $(".article1 .recommend1").css("display","none");
+    $(".article1 .recommend2").css("display","none");
+    $(".article1 .recommend3").css("display","none");
+    $(".article1 .recommend5").css("display","none");
+    $(".article1 .recommend6").css("display","none");
+    });
+$(".num5").on("click",function(){
+    $(".article1 .recommend5").css("display","block");
+    $(".article1 .recommend1").css("display","none");
+    $(".article1 .recommend2").css("display","none");
+    $(".article1 .recommend3").css("display","none");
+    $(".article1 .recommend4").css("display","none");
+    $(".article1 .recommend6").css("display","none");
+    });
+$(".num6").on("click",function(){
+    $(".article1 .recommend6").css("display","block");
+    $(".article1 .recommend1").css("display","none");
+    $(".article1 .recommend2").css("display","none");
+    $(".article1 .recommend3").css("display","none");
+    $(".article1 .recommend4").css("display","none");
+    $(".article1 .recommend5").css("display","none");
+    });
